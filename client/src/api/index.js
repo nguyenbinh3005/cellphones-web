6763012a6782b13d6e7847e365c47776b630d20b
Binh Le Nguyen <nguyenbinh3005.92@gmail.com>
2021-09-20 01:20:17 +0700

    change link api to deploy

--- client/src/api/index.js
+++ client/src/api/index.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const URL = 'http://localhost:5000';
+const URL = 'https://cellphones-web.herokuapp.com/';
 
 export const getproducts = () => axios.get(`${URL}/api/products`);
 export const getproductdetail = (title) =>
