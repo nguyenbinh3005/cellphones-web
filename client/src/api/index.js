92561c5c929e3ca9d4f9119942bcba9d6668df73
Binh Le Nguyen <nguyenbinh3005.92@gmail.com>
2021-09-20 01:33:35 +0700

    change link api

--- client/src/api/index.js
+++ client/src/api/index.js
@@ -1,8 +1,8 @@
 import axios from 'axios';
 
-const URL = 'http://localhost:5000';
+// const URL = 'http://localhost:5000';
 
-// const URL = 'https://cellphones-web.herokuapp.com';
+const URL = 'https://cellphones-web.herokuapp.com';
 
 export const getproducts = () => axios.get(`${URL}/api/products`);
 export const getproductdetail = (title) =>
